ad7be6a2581c18eb5d5deba59e5d012858033d6b
Fiorella🌺 <fiorellaqc17@gmail.com>
2026-04-26 11:55:23 +0200

    barra de navegacion

--- frontend/app/src/main/java/com/fiorella/plantguardian/ui/main/MainActivity.kt
+++ frontend/app/src/main/java/com/fiorella/plantguardian/ui/main/MainActivity.kt
@@ -1,12 +1,36 @@
 package com.fiorella.plantguardian.ui.main
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.fiorella.plantguardian.R
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val bottomNavigation = findViewById<BottomNavigationView>(R.id.bottomNavigation)
+        bottomNavigation.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.nav_home -> {
+                    true
+                }
+                R.id.nav_plants -> {
+                    Toast.makeText(this, "Mis plantas", Toast.LENGTH_SHORT).show()
+                    true
+                }
+                R.id.nav_add -> {
+                    Toast.makeText(this, "Añadir planta", Toast.LENGTH_SHORT).show()
+                    true
+                }
+                R.id.nav_profile -> {
+                    Toast.makeText(this, "Mi perfil", Toast.LENGTH_SHORT).show()
+                    true
+                }
+                else -> false
+            }
+        }
     }
 }
