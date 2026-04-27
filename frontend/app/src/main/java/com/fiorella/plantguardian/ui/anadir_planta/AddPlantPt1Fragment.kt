5a37dda5f061851be9ae0af525071f308bc58259
Fiorella🌺 <fiorellaqc17@gmail.com>
2026-04-27 19:09:24 +0200

    permisos de añadir foto

--- frontend/app/src/main/java/com/fiorella/plantguardian/ui/anadir_planta/AddPlantPt1Fragment.kt
+++ frontend/app/src/main/java/com/fiorella/plantguardian/ui/anadir_planta/AddPlantPt1Fragment.kt
@@ -1,15 +1,27 @@
 package com.fiorella.plantguardian.ui.anadir_planta
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.ImageView
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import com.fiorella.plantguardian.R
 import com.fiorella.plantguardian.ui.main.AddPlantFragment
 
 class AddPlantPt1Fragment : Fragment() {
+    private val camaraLauncher = registerForActivityResult(ActivityResultContracts.TakePicturePreview()) { bitmap: Bitmap? ->
+        if (bitmap != null) {
+            view?.findViewById<ImageView>(R.id.btnCamara)?.setImageBitmap(bitmap)
+            Log.d("CAMERA_DEBUG", "Foto capturada con éxito.")
+        }
+    }
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_add_plant_pt1, container, false)
     }
@@ -26,6 +38,15 @@ class AddPlantPt1Fragment : Fragment() {
 
         view.findViewById<Button>(R.id.btnSiguientePaso1).setOnClickListener {
             // Paso 2
+        }
+
+        view.findViewById<ImageView>(R.id.btnCamara).setOnClickListener {
+            val permissionCheck = ContextCompat.checkSelfPermission(requireContext(), android.Manifest.permission.CAMERA)
+            if (permissionCheck == PackageManager.PERMISSION_GRANTED) {
+                camaraLauncher.launch(null)
+            } else {
+                requestPermissions(arrayOf(android.Manifest.permission.CAMERA), 100)
+            }
         }
     }
 
